af3492abbf4de057971a01f9a4d786c23c90d7c5
Shahar Loebel <shahar.lebel@gmail.com>
2026-02-15 23:33:46 +0200

    first unit tests

--- src/lib/react-timeline.test.tsx
+++ src/lib/react-timeline.test.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vitest/globals" />
 import React from 'react';
 
 import { render } from '@testing-library/react';
@@ -6,17 +7,17 @@ import { getData } from './mocks/data';
 import ReactTimeline from './react-timeline';
 
 describe('ReactTimeline', () => {
-  it('should render successfully', () => {
-    const { rows, range } = getData();
-    const { baseElement } = render(
-      <ReactTimeline
-        rows={rows}
-        options={{
-          minAxisTime: range.start,
-          maxAxisTime: range.end,
-        }}
-      />
-    );
-    expect(baseElement).toBeTruthy();
-  });
+	it('should render successfully', () => {
+		const { rows, range } = getData();
+		const { baseElement } = render(
+			<ReactTimeline
+				rows={rows}
+				options={{
+					minAxisTime: range.start,
+					maxAxisTime: range.end
+				}}
+			/>
+		);
+		expect(baseElement).toBeTruthy();
+	});
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,11 @@
 import { configure } from '@testing-library/react';
 
 configure({ testIdAttribute: 'data-test-id' });
+
+// ResizeObserver is not implemented in jsdom
+class ResizeObserverMock {
+	observe = () => undefined;
+	unobserve = () => undefined;
+	disconnect = () => undefined;
+}
+global.ResizeObserver = ResizeObserverMock as typeof ResizeObserver;
